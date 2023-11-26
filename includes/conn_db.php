c50061a37bdbc03bdec6263d71d64f8d52e8fd53
devnacer <kalache.nacer.kn@gmail.com>
2023-11-26 18:56:55 +0100

    blogos

--- includes/conn_db.php
+++ includes/conn_db.php
@@ -2,7 +2,7 @@
 $host = "localhost"; // Database host name
 $dbname = "myblog"; // Database name
 $username = "root"; // Database username
-$password = "root"; // Database password
+$password = ""; // Database password
 
 try {
     // Create a new instance of the PDO (PHP Data Objects) class to establish a database connection.
