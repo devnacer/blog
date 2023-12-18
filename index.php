507405f89665d8cf8e2c21667363adfcb279c431
devnacer <kalache.nacer.kn@gmail.com>
2023-12-18 23:28:55 +0100

    18dec

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ require_once 'includes/functions.php';
 
 //////
 // select name of all categories
-$sqls = $pdo->prepare('SELECT name FROM category');
+$sqls = $pdo->prepare('SELECT * FROM category');
 $sqls->execute();
-$categoryNames = $sqls->fetchAll(PDO::FETCH_ASSOC);
+$categories = $sqls->fetchAll(PDO::FETCH_ASSOC);
 
 // last article
 $sqlstat = $pdo->prepare('SELECT * FROM article ORDER BY id DESC LIMIT 1');
@@ -37,9 +37,13 @@ $sqlstate = $pdo->prepare('SELECT name FROM category WHERE id = ?');
 $sqlstate->execute([$idCategory]);
 $nameCategory = $sqlstate->fetch(PDO::FETCH_ASSOC);
 
-// echo '<pre>';
-// var_dump($nameCategory);
-// echo '</pre>';
+// select articles
+
+$articles = $pdo->query('SELECT * FROM article ORDER BY date_creation')->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+var_dump($articles);
+echo '</pre>';
 ?>
 
 <div class="container">
@@ -108,12 +112,60 @@ $nameCategory = $sqlstate->fetch(PDO::FETCH_ASSOC);
     <!-- categories -->
     <div class="py-1 mb-2">
      <ul class="list-group">
-         <?php foreach($categoryNames as $itemCategoryName):?>
+         <?php foreach($categories as $itemCategory):?>
+            <?php
+            $itemCategoryId = $itemCategory['id'];
+            $slqsNbcateByArticle = $pdo->prepare('SELECT COUNT(*) as articleCount
+                                                 FROM article 
+                                                 WHERE id_category = ?');
+            $slqsNbcateByArticle->execute([$itemCategoryId]);
+            $nbArticleByCategory = $slqsNbcateByArticle->fetch(PDO::FETCH_ASSOC);
+            ?>
              <li class="list-group-item">
-                 <a class="#" href="#"><?= $itemCategoryName['name']?></a>
+                <a class="#" href="#"><?= $itemCategory['name']?> 
+                    <span class='"badge bg-light text-dark'>
+                        <?= '('.$nbArticleByCategory['articleCount'].')'?>articles
+                    </span>
+                </a>
              </li>
          <?php endforeach?>
      </ul> 
     </div>
+
+    <!-- articles -->
+    <?php foreach($articles as $article):?>
+    <?php
+        //select category
+        $idCate = $article['id_category'];
+        $sqlstateCate = $pdo->prepare('SELECT name FROM category WHERE id = ?');
+        $sqlstateCate->execute([$idCate]);
+        $nameCate = $sqlstateCate->fetch(PDO::FETCH_ASSOC);
+        // Published
+        $pubDate = $article['date_creation']; 
+        $elapsedTime = time_elapsed_string($pubDate);
+        // select author
+        $idAuth = $article['id_author'];
+        $sqlsAuth = $pdo->prepare('SELECT adminName FROM admin WHERE id = ?');
+        $sqlsAuth->execute([$idAuth]); 
+        $nameAuth = $sqlsAuth->fetch(PDO::FETCH_ASSOC);
+    ?>
+    <div class="card" style="width: 18rem;">
+        <img src="uploaded/<?= $article['id_image']?>" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?= $article['title']?></h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Category: <strong><?= $nameCate['name'] ?></strong></li>
+            <li class="list-group-item">Published <strong><?= $elapsedTime ?></strong> by <strong><?= $nameAuth['adminName'] ?></strong></li>
+        </ul>
+        <div class="card-body">
+            <a href="article.php?id=<?= $article['id'] ?>" class="btn btn-primary d-flex justify-content-center">Read More</a>
+
+    </div>
+    <?php endforeach?>
+
+
+</div>
     
 </main>
